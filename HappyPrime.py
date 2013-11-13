0be460fb1ba26b2a599f2e2afe7c6fb53ee3d5c8
Juha Autero <jautero@iki.fi>
2013-11-13 18:25:44 +0200

    First passing unit test.

---
+++ HappyPrime.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+# 
+#  HappyPrime.py
+#  HappyPrime
+#
+#  Create happy primes. Happy primes are cool.  (I know, that's 11th doctor, not 10th)
+#  Created by Juha Autero on 2013-11-13.
+#  Copyright 2013 Juha Autero. All rights reserved.
+# 
+
+def getNextHappyPrime(happy_prime):
+    return 331
